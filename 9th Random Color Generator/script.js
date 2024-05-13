8bbec0adb23d032b7c463106b934b2d8f9fc6ea9
itsabdullah15 <itsabdullah.cg@gmail.com>
2024-05-13 06:57:36 +0530

    2.0

--- 9th Random Color Generator/script.js
+++ 9th Random Color Generator/script.js
@@ -19,21 +19,35 @@ hexBtn.addEventListener("click", () => {
   hexBtn.style.color = `#${hexColorOutput}`;
 });
 
+// RGB Color generator
+const rgbBtn = document.querySelector(".rgb-btn");
+const getRedInputRange = document.querySelector("#red");
+const getGreenInputRange = document.querySelector("#green");
+const getBlueInputRange = document.querySelector("#blue");
+const rgbColorContainer = document.querySelector(".rgb-color-container");
+const rgbCopyBtn = document.querySelector(".rgb-copy-color");
+const rgbColorValue = document.querySelector(".rgb-color-value");
 
-//RGB Color generator
-const rgbBtn = document.querySelector('.rgb-btn');
-const getRedInputRange = document.querySelector('#red');
-const getGreenInputRange = document.querySelector('#green');
-const getBlueInputRange = document.querySelector('#blue');
-const rgbColorContainer = document.querySelector('.rgb-color-container');
+rgbBtn.addEventListener("click", () => {
+  let extractRedValue = getRedInputRange.value;
+  let extractGreenValue = getGreenInputRange.value;
+  let extractBlueValue = getBlueInputRange.value;
 
-
-rgbBtn.addEventListener('click', ()=> {
- let extractRedValue = getRedInputRange.value ;
- let extractGreenValue = getGreenInputRange.value ;
- let extractBlueValue = getBlueInputRange.value ;
-
- rgbColorContainer.style.background = `rgb(${extractRedValue}, ${extractGreenValue}, ${extractBlueValue})`
- rgbBtn.style.color = `rgb(${extractRedValue}, ${extractGreenValue}, ${extractBlueValue})`
-
+  rgbColorValue.textContent = `rgb(${extractRedValue}, ${extractGreenValue}, ${extractBlueValue})`;
+  rgbColorContainer.style.backgroundColor = `rgb(${extractRedValue}, ${extractGreenValue}, ${extractBlueValue})`;
+  rgbBtn.style.color = `rgb(${extractRedValue}, ${extractGreenValue}, ${extractBlueValue})`;
 });
+
+function copyHexColorToClipBoard() {
+  navigator.clipboard.writeText(hexColorValue.textContent);
+  alert("Hex Color is copied to clipboard");
+}
+
+hexCopyBtn.addEventListener("click", copyHexColorToClipBoard);
+
+function copyRgbColorToClipboard() {
+  navigator.clipboard.writeText(rgbColorValue.textContent);
+  alert("RGB Color is copied to clipboard");
+}
+
+rgbCopyBtn.addEventListener("click", copyRgbColorToClipboard);
